2f64189af3de6fa54f6700ca8a4f07a8b475d813
franç <franjseco@gmail.com>
2021-12-28 10:33:01 +0100

    Prueba

--- LucaSteam_G02/src/control/Main.java
+++ LucaSteam_G02/src/control/Main.java
@@ -7,9 +7,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Cambio");
-		System.out.println("Ayuda diosito");
-		System.out.println("Diosito: morios con eclipse, infieles");
+		System.out.println("Esta es una prueba");
+
 
 	}
 
