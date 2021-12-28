9ff6123019aec42418ffb5ece57d634f9db76213
Apulsar <themrmike98@hotmail.es>
2021-12-28 14:00:15 +0100

    Añadido mainMenu a control, añadido arranque de la plataforma a Main, añadido showMenu a la clase Menu de gui y testing de mainMenu

--- LucaSteam_G02/src/control/Main.java
+++ LucaSteam_G02/src/control/Main.java
@@ -7,8 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Esta es una prueba");
-
+		new PlatformGames().startPlatform();
 
 	}
 
--- LucaSteam_G02/src/gui/Menu.java
+++ LucaSteam_G02/src/gui/Menu.java
@@ -3,7 +3,12 @@ package gui;
 public class Menu {
 	
 	public static void showMenu() {
-		
+		System.out.println("---------------------------------");
+		System.out.println("           LUCASTEAM             ");
+		System.out.println("---------------------------------");
+		System.out.println("");
+		System.out.println("     ¿Qué desea hacer?");
+		System.out.println("       1.Consultar lista de todos los juegos  ");
 	}
 
 }
