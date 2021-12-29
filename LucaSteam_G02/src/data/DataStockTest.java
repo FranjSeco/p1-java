6842ee17323729517e9c108b63cd2a70a6767ba5
Apulsar <themrmike98@hotmail.es>
2021-12-29 19:42:05 +0100

    Esbozo testReadData

--- LucaSteam_G02/src/data/DataStockTest.java
+++ LucaSteam_G02/src/data/DataStockTest.java
@@ -2,13 +2,21 @@ package data;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+
 import model.Game;
 class DataStockTest {
 
@@ -84,6 +92,54 @@ class DataStockTest {
 		//Then
 		assertEquals(AddProducto(codigoJuego2,juego2),false);
 		
+	}
+	public Map<Integer, Game> ReadData() {
+
+
+		String fileName = "./archivoFinal2.csv";
+
+		List<String[]> r;
+		try (CSVReader reader = new CSVReader(new FileReader(fileName))) {
+			r = reader.readAll();
+			int listIndex = 0;
+			for (String[] arrays : r) {
+				Game a = new Game(arrays[0], arrays[1], arrays[2], arrays[3], arrays[4]);
+				this.AddProducto(listIndex, a);
+				listIndex++;
+			}
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Error");
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (CsvException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return juegos;
+
+	}
+	
+	@Test
+	void testReadData() {
+		//Given
+		//un archivo de texto
+		
+		//When
+		//Le paso ese archivo al metodo ReadData
+		Map<Integer,Game> pepito=new HashMap<>();
+		Game juego1=new Game("FIFA16","PS3","2016","Sports","EA");
+		Game juego2=new Game("COD","PS3","2015","Shooter","Activision");
+		Game juego3=new Game("LoL","PC","2010","Sports","Riot");
+		pepito.put(0,juego1);
+		//Then
+		//Asegúrame que el método crea el mapa igual que si lo creo yo a mano
+		assertEquals(ReadData(),pepito);
 	}
 
 }
